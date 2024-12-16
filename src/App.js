59461ffcce8035c7abe75f82bf5d9dbe0ba15f68
Ansh <anshsrivasrava2004@gmail.com>
2024-12-17 00:55:39 +0530

    Merged signup with landing page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-<<<<<<< HEAD
-import React from 'react';
-import {Routes,Route} from 'react-router-dom'
-import LandingPage from './pages/LandingPage';
-import Result from './pages/Result';
-import BuyCredit from './pages/BuyCredit';
-import { Form } from 'react-router-dom';
-import Navbar from './components/Landing/Navbar';
-
-function App() {
-  return (
-    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b frm-teal-50 to-orange-50'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-        <Route path='/result' element={ <Result/>}/>
-        <Route path='/BuyCredit' element={<BuyCredit/>}/>
-      </Routes>
-=======
 import React, { useContext } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage';
 import Result from './pages/Result';
 import BuyCredit from './pages/BuyCredit';
 import Navbar from './components/Landing/Navbar';
-import Login from './components/Login'; 
 import { AppContext } from './context/AppContext'; 
+import SignUp from './components/SignUp';
 
 function App() {
   const { showLogin } = useContext(AppContext); 
@@ -38,8 +19,7 @@ function App() {
         <Route path='/BuyCredit' element={<BuyCredit/>}/>
       </Routes>
       
-      {showLogin && <Login />}
->>>>>>> 67158c2 (my commit)
+      {showLogin && <SignUp />}
     </div>
   );
 }
