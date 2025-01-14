51457b2d250ba1766ff5280dfcf8a1c7b2998786
Soumyadip Maity <22053029@kiit.ac.in>
2025-01-15 01:44:36 +0530

    Enhance Footer component with social media icons and updated copyright link

--- frontend/src/components/Landing/Footer.js
+++ frontend/src/components/Landing/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import { SiLinkedin, SiInstagram } from 'react-icons/si'
+import { HiGlobeAlt } from 'react-icons/hi'
 
 export default function Footer() {
   return (
@@ -9,44 +11,32 @@ export default function Footer() {
         <img src={assets.Logo} width={150} alt="Logo"/>
       </div>
 
-      <p className='text-sm text-gray-600 w-full text-center sm:w-auto sm:text-left'>Copyright @USCKIIT | All right reserved.</p>
+      <p className='text-sm text-gray-600 w-full text-center sm:w-auto sm:text-left'>Copyright @<span><a href="https://usckiit.in" target="_blank" className='text-blue-600 hover:underline'>USC KIIT</a></span> | All right reserved.</p>
 
-      <div className='flex gap-4 w-full sm:w-auto justify-center sm:justify-end'>
+      <div className='flex gap-6 w-full sm:w-auto justify-center sm:justify-end'>
         <a 
           href="https://www.linkedin.com/company/uipath-community-kiit/posts/?feedView=all" 
           target="_blank" 
           rel="noopener noreferrer"
+          className='text-gray-600 hover:text-blue-600 transform hover:scale-110 transition-all duration-200'
         >
-          <img 
-            src={assets.linkedin} 
-            alt='Facebook' 
-            width={35} 
-            className='cursor-pointer hover:opacity-80 transition-opacity'
-          />
+          <SiLinkedin size={26} />
         </a>
         <a 
           href="https://www.instagram.com/usc.kiit/" 
           target="_blank" 
           rel="noopener noreferrer"
+          className='text-gray-600 hover:text-pink-600 transform hover:scale-110 transition-all duration-200'
         >
-          <img 
-            src={assets.instagram_icon} 
-            alt='Instagram' 
-            width={35} 
-            className='cursor-pointer hover:opacity-80 transition-opacity'
-          />
+          <SiInstagram size={26} />
         </a>
         <a 
-          href="https://twitter.com/usckiit" 
+          href="https://usckiit.in" 
           target="_blank" 
           rel="noopener noreferrer"
+          className='text-gray-600 hover:text-green-600 transform hover:scale-110 transition-all duration-200'
         >
-          <img 
-            src={assets.twitter_icon} 
-            alt='Twitter' 
-            width={35} 
-            className='cursor-pointer hover:opacity-80 transition-opacity'
-          />
+          <HiGlobeAlt size={28} />
         </a>
       </div>
     </div>
